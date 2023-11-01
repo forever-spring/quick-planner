1fb5b63f0bcd6fa9278421ea1e2973144a184063
baharp <bpaydar92@gmail.com>
2023-11-01 16:22:58 +0330

    fixed direction

--- components/tour.js
+++ components/tour.js
@@ -9,9 +9,11 @@ import { settingsContext } from "../assets/utils/settings";
 
 export default function Tour({endAction}){
 	const theme = useContext(settingsContext).DarkTheme ? 'dark' : 'light';
+	const lang = useContext(settingsContext).AppLanguage;
+	const dir = lang in ['fa']?'rtl':'ltr';
 
 	return (
-		<View style={containers[theme]}>
+		<View style={{...containers[theme],...containers[dir]}}>
 			<TextButton label='Start App' action={endAction} style={styles.button} />
 		</View>
 	);
